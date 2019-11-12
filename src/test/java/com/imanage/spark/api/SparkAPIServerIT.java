5ed234f33cabb339257016218911434c79f3c2df
Jagadeesh Kothuri <jagadeesh.kothuri@jkothuri-mbp.local>
2019-11-12 12:47:11 -0600

    integration tests

---
+++ src/test/java/com/imanage/spark/api/SparkAPIServerIT.java
@@ -0,0 +1,100 @@
+package com.imanage.spark.api;
+
+import com.despegar.http.client.GetMethod;
+import com.despegar.http.client.HttpClientException;
+import com.despegar.http.client.HttpResponse;
+import com.despegar.http.client.PostMethod;
+import com.despegar.sparkjava.test.SparkServer;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Test;
+import spark.servlet.SparkApplication;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SparkAPIServerIT {
+
+    public static class SparkAPIServerTestApp implements SparkApplication{
+        public void init(){
+            new SparkAPIServer().api();
+        }
+    }
+
+    @ClassRule
+    public static SparkServer<SparkAPIServerTestApp> testServer = new SparkServer<>(SparkAPIServerTestApp.class, 8080);
+
+    @Test
+    public void serverRespondsSuccessfully() throws HttpClientException {
+        GetMethod request = testServer.get("/", false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(200, httpResponse.code())
+        );
+    }
+
+    @Test
+    public void testHello() throws HttpClientException {
+        GetMethod request = testServer.get("/", false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(200, httpResponse.code()),
+                () -> assertEquals(("Hello !!!").getBytes().length,httpResponse.body().length)
+        );
+    }
+
+    @Test
+    public void testHelloWithNoParam() throws HttpClientException {
+        GetMethod request = testServer.get("/hello/", false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(404, httpResponse.code())
+        );
+    }
+
+    @Test
+    public void testHelloWithParam() throws HttpClientException {
+        String requestParam="Chicago";
+        GetMethod request = testServer.get("/hello/"+requestParam, false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(200, httpResponse.code()),
+                () -> assertEquals(("Good Morning "+requestParam).getBytes().length,httpResponse.body().length)
+        );
+
+    }
+
+    @Test
+    public void testPostAMessage() throws HttpClientException {
+        String requestBody="{\"text\":\"this is the text of the message\",\"from\":\"dave\",\"to\":\"jagadeesh\"}";
+        String responseBody="{\"id\":1,\"text\":\"this is the text of the message\",\"from\":\"dave\",\"to\":\"jagadeesh\"}";
+        PostMethod request=testServer.post("/messages",requestBody,false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(200, httpResponse.code()),
+                () -> assertEquals(responseBody.getBytes().length,httpResponse.body().length)
+        );
+    }
+
+    @Test
+    public void testGetExistingMessage() throws HttpClientException {
+        String requestParam="1";
+        String responseBody="{\"id\":1,\"text\":\"this is the text of the message\",\"from\":\"dave\",\"to\":\"jagadeesh\"}";
+        GetMethod request = testServer.get("/messages/"+requestParam, false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(200, httpResponse.code()),
+                () -> assertEquals(responseBody.getBytes().length,httpResponse.body().length)
+        );
+    }
+
+    @Test
+    public void testGetMessageThatDoesNotExist() throws HttpClientException {
+        String requestParam="10";
+        GetMethod request = testServer.get("/messages/"+requestParam, false);
+        HttpResponse httpResponse = testServer.execute(request);
+        assertAll(
+                () -> assertEquals(404, httpResponse.code()),
+                () -> assertEquals("Message not found".getBytes().length,httpResponse.body().length)
+        );
+    }
+}
---
+++ src/test/java/com/imanage/spark/data/MessageTest.java
@@ -0,0 +1,21 @@
+package com.imanage.spark.data;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MessageTest {
+    @Tag("DEV")
+    @Test
+    void testMessage(){
+        Message message=new Message("hello","Matt","Chris");
+        assertAll(
+                ()->assertTrue(true),
+                ()->assertNotNull(message),
+                ()->assertTrue(message.getId()==0)
+        );
+    }
+}
